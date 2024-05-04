a2e0251f0e95350a0216a06493dc90b5a30d45ba
e3emmaworld <168737077+e3emmaworld@users.noreply.github.com>
2024-05-04 16:26:51 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+window.alert("Welcome to E3'S Emmah's World Tech");
+
+
+function startTime () {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById("time").innerHTML = h + ":" +  m  + ":" + s;
+    setTimeout(startTime, 1000);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};
+    return i;
+}
